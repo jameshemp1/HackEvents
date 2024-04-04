4c5f4550a5de97e31217e53efa79bd9906661bec
Raidanor <silverstallion30@gmail.com>
2024-04-04 07:41:03 +0400

    added navbars to all 3 pages

--- update.php
+++ update.php
@@ -4,10 +4,33 @@
         <title>update.php</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
     </head>
-    <nav> 
+
+    <!-- <nav> 
         <a style="margin-left: 5px" href="./index.php">Home</a> 
         <a href="./delete.php">Delete Event</a>
+    </nav> -->
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="./index.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="./delete.php">Delete Event</a>
+                </li>
+                
+            </ul>
+            <span class="navbar-text">
+                
+            </span>
+        </div>
     </nav>
+
         <body style="background-color: #faebd7;">
         <div id="cond">
         <?
